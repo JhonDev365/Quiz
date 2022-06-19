a7e0c6a4494896d66d74087e2ec68d080570053b
JhonDev365 <jhonarcila365@gmail.com>
2022-06-19 16:18:45 -0500

    Add questions

---
+++ data/data.js
@@ -0,0 +1,21 @@
+export const data = [
+    {question: '¿Cual es la capital de Finlandia?',
+     choices: ['Helsinky', 'Otanfur', 'Rovaniemi', 'Turku'],
+     answer: 'Helsinki'},
+    
+    {question: '¿Cual es la capital de Grecia?',
+     choices: ['Corinto', 'Atenas', 'Salonica', 'Esparta'],
+     answer: 'Atenas'},
+
+     {question: '¿Cual es la capital de Polonia?',
+     choices: ['Varsovia', 'Cracovia', 'Lublin', 'Turin'],
+     answer: 'Varsovia'},
+     
+     {question: '¿Cual es la capital de Bulgaria?',
+     choices: ['Burgas', 'Pleven', 'Sofia', 'Nesebar'],
+     answer: 'Sofia'},
+
+     {question: '¿Cual es la capital de Rumanía?',
+     choices: ['Brasot', 'Iasi', 'Bucarest', 'Arad'],
+     answer: 'Bucarest'},
+];
