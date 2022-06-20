8e3a2f44d3f178e9661b96680edf6ca89591bd37
JhonDev365 <jhonarcila365@gmail.com>
2022-06-20 11:37:02 -0500

    Add metods

--- main.js
+++ main.js
@@ -1,2 +1,24 @@
-import { data } from "./data/data.js";
+import { questions } from "./data/questions.js";
+import { Quiz } from "./models/Quiz.js";
+import { UI } from "./models/UI.js";
+
+const renderPage = (quiz, ui) => {
+    if(quiz.isEnded()) {
+        console.log('finalizo');
+    } else {
+        ui.showQuestion(quiz.getQuestionIndex().text);
+    ui.showChoices(quiz.getQuestionIndex().choices, (currentChoice) => {
+    quiz.guess(currentChoice);
+    renderPage(quiz, ui);
+    });
+    }
+}
+
+function main() {
+    const quiz = new Quiz(questions);
+    const ui = new UI();
+    renderPage(quiz, ui);
+}
+
+main();
 
